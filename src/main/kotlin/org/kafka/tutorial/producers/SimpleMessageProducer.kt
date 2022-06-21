b871904884822811680e7ea58bcd8fec814c580a
Sinnappu Jeyakumaran <Sinnappu.Jeyakumaran@reece.com.au>
2022-06-21 16:15:56 +1000

    Adding bookclub avro schema

--- src/main/kotlin/org/kafka/tutorial/producers/SimpleMessageProducer.kt
+++ src/main/kotlin/org/kafka/tutorial/producers/SimpleMessageProducer.kt
@@ -9,7 +9,6 @@ import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
 import org.springframework.util.concurrent.ListenableFutureCallback
-import java.io.File
 
 private val log = KotlinLogging.logger {}
 
@@ -17,10 +16,37 @@ private val log = KotlinLogging.logger {}
 class SimpleMessageProducer
 @Autowired constructor(private val kafkaTemplate: KafkaTemplate<String, GenericData.Record>) {
 
+    val bookClubSchemaString = """
+        {
+          "type": "record",
+          "namespace": "BookClub",
+          "name": "Book",
+          "fields": [
+            {
+              "name": "Name",
+              "type": "string"
+            },
+            {
+              "name": "Author",
+              "type": "string"
+            },
+            {
+              "name": "Genre",
+              "type": "string"
+            },
+            {
+              "name": "Rating",
+              "type": "int"
+            }
+          ]
+        }
+    """.trimIndent();
+
     @Value(value = "\${message.topic.name}")
     private val topicName: String? = null
     fun sendMessage(message: String) {
-        val bookClubSchema = Schema.Parser().parse(File(SimpleMessageProducer::class.java.getResource("/BookClub.avsc").toURI()))
+//        val bookClubSchema = Schema.Parser().parse(File(SimpleMessageProducer::class.java.getResource("/BookClub.avsc").toURI()))
+        val bookClubSchema = Schema.Parser().parse(bookClubSchemaString)
         val bookClub = GenericData.Record(bookClubSchema)
         bookClub.put("Name", "test");
         bookClub.put("Author", "test");
